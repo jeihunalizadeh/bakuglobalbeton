7d5db19b3be52cfda950287394fa453528c642a7
rasulagarzayev1 <agarzayev.rasul@gmail.com>
2018-07-26 21:32:08 +0400

    navbar and footer

---
+++ src/js/script.js
@@ -0,0 +1,85 @@
+$(document).ready(function(){
+    $(".search").click(function(){
+        $(".searchinput").css({
+            "display":"inline-block"
+        })
+        $(".inputclose").css({
+            "display":"inline-block"
+        })
+    })
+    $(".inputclose").click(function(){
+        $(".searchinput").css({
+            "display":"none"
+        })
+        $(this).css({
+            "display":"none"
+        })
+    })
+    $(".firstsection").click(function(){
+        $(".searchinput").css({
+            "display":"none"
+        })
+        $(".inputclose").css({
+            "display":"none"
+        })
+    })
+    $(".menuicn").click(function(){
+        $(this).css({
+            "display":"none"
+        })
+        $(".menulayout").css({
+            "width":"100%",
+            "height":"auto"
+        })
+        $(".cancel").css({
+            "display":"block",
+        })
+    })
+    $(".cancel").click(function(){
+        $(".menulayout").animate({
+            "width":"0"
+        }, 1700, function(){
+            $(".menulayout").css({
+                "height":"0"
+            })
+        })
+        $(this).css({
+            "display":"none"
+        })
+        $(".menuicn").css({
+            "display":"block"
+        })
+    })
+        let shareclick=document.querySelector(".shareclick");
+        let shareDiv=document.querySelector(".shareDiv");
+        shareclick.onclick=function(){
+                if(shareDiv.style.display=="inline-block"){
+                    shareDiv.style.display="none";
+                }
+                else{
+                    shareDiv.style.display="inline-block";
+                }
+                
+            }
+            
+            $('.owl-carousel').owlCarousel({
+                loop:true,
+                margin:10,
+                nav:false,
+                autoplay:true,
+                responsive:{
+                    0:{
+                        items:1
+                    },
+                    600:{
+                        items:3
+                    },
+                    1000:{
+                        items:5
+                    }
+                }
+            })       
+
+
+
+ });
